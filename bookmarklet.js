e811daca4035415e919857809c83f8c8d4fe18cb
Mark Kazanski <markkazanski@gmail.com>
2018-01-14 17:22:49 -0600

    changed bookmarketl

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,29 +1,10 @@
-var mysql = require("mysql");
+var newURL = document.URL;
+console.log("newURL: " + newURL);
 
-var newURL = process.argv[2];
-
-console.log(newURL); 
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "Arseface04!",
-  database: "bookmarklet_db"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-
-  connection.query(`INSERT INTO bookmarks (url) VALUES ("${newURL}")`, function(err, res){
-      if(err) throw err;
-
-      console.log(res);
-      connection.destroy();
+$.ajax({
+    url: "https://httpbin.org/post",
+    method: "POST",
+    data: {newURL: newURL }
+  }).done(function(response) {
+    console.log(response);
   });
-});
