a909fb785da14233219e1d9e5e121ebbf4d98169
Mark Kazanski <markkazanski@gmail.com>
2018-01-14 18:57:25 -0600

    cors

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -1,6 +1,15 @@
 var newURL = document.URL;
 console.log("newURL: " + newURL);
 
+// ===============CORS CODE=========================================
+jQuery.ajaxPrefilter(function(options) {
+  if (options.crossDomain && jQuery.support.cors) {
+      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+  }
+});
+// =================================================================        
+
+
 $.ajax({
     url: "https://limitless-refuge-66019.herokuapp.com/api/new",
     method: "POST",
