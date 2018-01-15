c34ded136c27e19760faa6dc31248673991f0ebe
Mark Kazanski <markkazanski@gmail.com>
2018-01-14 18:49:02 -0600

    port

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -2,7 +2,7 @@ var newURL = document.URL;
 console.log("newURL: " + newURL);
 
 $.ajax({
-    url: "http://localhost:3000/api/new",
+    url: "https://limitless-refuge-66019.herokuapp.com/api/new",
     method: "POST",
     data: {newURL: newURL }
   }).done(function(response) {
