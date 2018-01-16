96b091bd64843745299c63b1c4e0b9f9963c44d5
Mark Kazanski <markkazanski@gmail.com>
2018-01-15 23:05:03 -0600

    styling

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -30,8 +30,8 @@ $.ajax({
 }).done(function(response) {
   //alert("Added your page to the database. " + response);
   $("body").append($("<div id='modal'>"));
-  $("#modal").css({'width':'100%','height':'200px','background-color':'blue','position':'fixed','top':'0','left':'0', 'display':'grid', 'color':'#fff', 'text-align':'center'});
-  $("#modal").append(`<h1>Your Link Has Been Added: ${newURL}</h1>`);
-  setTimeout(function(){$("#modal").hide()}, 4000);
+  $("#modal").css({'width':'100%','height':'200px','background-color':'#e44125','position':'fixed','top':'50%','z-index':'999999', 'color':'#eee5d0', 'text-align':'center'});
+  $("#modal").append(`<h1>Your Link Has Been Added:<br /> ${newURL}</h1>`);
+  setTimeout(function(){$("#modal").hide()}, 6000);
   console.log(response);
 });
