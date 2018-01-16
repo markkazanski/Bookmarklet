18708fe13bd37c41648aaf6e544324eab66f0677
Mark Kazanski <markkazanski@gmail.com>
2018-01-15 22:59:03 -0600

    added modal

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -17,7 +17,7 @@ $.ajax({
     crossDomain: true,
     data: {newURL: newURL }
   }).done(function(response) {
-    alert("Added your page to the database. " + response);
+    //alert("Added your page to the database. " + response);
     console.log(response);
   });
 */
@@ -28,6 +28,10 @@ $.ajax({
   method: "GET",
   crossDomain: true
 }).done(function(response) {
-  alert("Added your page to the database. " + response);
+  //alert("Added your page to the database. " + response);
+  $("body").append($("<div id='modal'>"));
+  $("#modal").css({'width':'100%','height':'200px','background-color':'blue','position':'fixed','top':'0','left':'0', 'display':'grid', 'color':'#fff', 'text-align':'center'});
+  $("#modal").append(`<h1>Your Link Has Been Added: ${newURL}</h1>`);
+  setTimeout(function(){$("#modal").hide()}, 4000);
   console.log(response);
 });
