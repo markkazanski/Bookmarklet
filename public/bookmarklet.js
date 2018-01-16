64afcabb39dc72d486be2aaa6e89f6aa3ce5069b
Mark Kazanski <markkazanski@gmail.com>
2018-01-15 23:25:09 -0600

    reload

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -8,6 +8,7 @@ if(!window.jQuery){
   var script = document.createElement('script');
   script.src = '//code.jquery.com/jquery-1.11.0.min.js';
   document.getElementsByTagName('head')[0].appendChild(script); 
+  location.reload();
 }
 
 // ===============CORS CODE=========================================
@@ -39,8 +40,8 @@ $.ajax({
 }).done(function(response) {
   //alert("Added your page to the database. " + response);
   $("body").append($("<div id='modal'>"));
-  $("#modal").css({'width':'100%','height':'200px','background-color':'#e44125','position':'fixed','top':'50%','z-index':'999999', 'color':'#eee5d0', 'text-align':'center'});
+  $("#modal").css({'width':'100%','height':'200px','background-color':'#e44125','position':'fixed','top':'50%','z-index':'999999', 'color':'#eee5d0', 'text-align':'center', 'display':'block'});
   $("#modal").append(`<h1>Your Link Has Been Added:<br /> ${newURL}</h1>`);
-  setTimeout(function(){$("#modal").hide()}, 6000);
+  setTimeout(function(){$("#modal").remove()}, 6000);
   console.log(response);
 });
